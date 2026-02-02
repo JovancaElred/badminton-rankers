02059ee2a51a60d104b5f141632c540c163d5c95
JovancaElred <jovanca.bonfilio@binus.ac.id>
2026-02-02 23:07:00 +0700

    adding recyclerview for member list

---
+++ app/src/main/java/com/example/badmintonrankers/view/adapter/PlayerAdapter.kt
@@ -0,0 +1,65 @@
+package com.example.badmintonrankers.view.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.example.badmintonrankers.R
+import com.example.badmintonrankers.data.model.Players
+
+class PlayerAdapter(
+    val onClick: (Players) -> Unit
+) : ListAdapter<Players, PlayerAdapter.PlayerViewHolder>(PlayersCallBack) {
+
+    class PlayerViewHolder(itemView: View, val onClick: (Players) -> Unit):RecyclerView.ViewHolder(itemView){
+        val memberRank : ImageView = itemView.findViewById(R.id.memberRank)
+        val memberName : TextView = itemView.findViewById(R.id.memberName)
+
+        var currItem : Players? = null
+
+        init {
+            itemView.setOnClickListener{
+                currItem?.let{
+                    onClick(it)
+                }
+            }
+        }
+
+
+        fun bind(players: Players){
+            currItem = players
+            memberName.text = players.displayName
+            Glide.with(itemView.context).load(players.rankImageLarge).error(R.drawable.account).centerCrop().into(memberRank)
+
+        }
+
+    }
+    fun setFilteredList(list: List<Players>){
+        submitList(list)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlayerAdapter.PlayerViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.row_members,parent,false)
+        return PlayerViewHolder(view, onClick)
+    }
+
+    override fun onBindViewHolder(holder: PlayerAdapter.PlayerViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
+
+object PlayersCallBack : DiffUtil.ItemCallback<Players>(){
+    override fun areItemsTheSame(oldItem: Players, newItem: Players): Boolean {
+        return oldItem.uuid == newItem.uuid
+    }
+
+    override fun areContentsTheSame(oldItem: Players, newItem: Players): Boolean {
+        return oldItem == newItem
+    }
+
+}
